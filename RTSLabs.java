f30cb2dc632befa50b09a97af018d0df8673c545
ovtsa <jobens@appstate.edu>
2020-09-08 14:57:55 -0400

    Q2

--- RTSLabs.java
+++ RTSLabs.java
@@ -17,12 +17,31 @@ public class RTSLabs {
 		System.out.printf("above: %d, below: %d\n", above, below);
 	}
 
+	/** Q2 - takes a string as input and "rotates" it by a set
+	 * amount. "Rotating" a string is moving a set number of characters
+	 * from the end back to the beginning of the string.
+	 *
+	 * @param s - the string to be rotated
+	 * @param r - the number of characters to be rotated
+	 * @return - the string post-rotation
+	 */
+	public static String rotateString(String s, int r) {
+		/* newFirstPiece = the substring from the END of String s that
+		has been moved to the beginning. */
+		String newFirstPiece = s.substring(s.length() - r % s.length(), s.length());
+		/* newSecondPiece = the substring from the BEGINNING of String s that
+		has now been pushed to the end. */
+		String newSecondPiece = s.substring(0, s.length() - r % s.length());
+		return newFirstPiece + newSecondPiece;
+	}
+
 	/** Main method
 	 *
 	 * @param args - command line arguments (unused)
 	 */
 	public static void main(String[] args) {
 		arrayAboveBelow(new int[]{1, 5, 2, 1, 10}, 6);
+		System.out.println(rotateString("MyString", 10));
 	}
 }
 
